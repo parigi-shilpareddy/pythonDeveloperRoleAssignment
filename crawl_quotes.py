124a8628b2ee31d0012b5cc9c8ed17da85ff00ce
parigi-shilpareddy <107068930+parigi-shilpareddy@users.noreply.github.com>
2022-08-01 15:59:37 +0530

    Add files via upload

--- crawl_quotes.py
+++ crawl_quotes.py
@@ -18,6 +18,7 @@ pages = [
 
 
 data = []
+authorsData = []
 for page in pages:
     r = requests.get(page)
     soup = BeautifulSoup(r.content, 'html.parser')
@@ -25,20 +26,32 @@ for page in pages:
     rows = soup.select('.quote')
     for i in range(len(rows)):
         d = dict()
+        a = dict()
         row = rows[i]
         d['quote'] = row.select_one('.text').text.strip()
         d['author'] = row.select_one('.author').text.strip()
         d['tags'] = row.find('meta')['content'].split(",")
+        quotes_page = row.select_one('.quote a')['href']
+        quotes_page = 'http://quotes.toscrape.com/' + quotes_page+'/'
+        author_request = requests.get(quotes_page)
+        author_soup = BeautifulSoup(author_request.content,'html.parser')
+        authors_born_date = author_soup.select_one('.author-born-date').text.strip()
+        authors_born_country = author_soup.select_one('.author-born-location').text.strip()
+        born = authors_born_date+' '+authors_born_country
+        a['name'] = row.select_one('.author').text.strip() 
+        a['born'] = born
+        a['reference'] = quotes_page
         data.append(d)
+        authorsData.append(a)
     sleep(10)
     
 quotes = dict()
 quotes['quotes'] = data
+quotes['authors'] = list(set(authorsData))
 
 with open('quotes.json', 'w') as f:
     json.dump(quotes, f)
 
 with open('quotes.json', 'r') as f:
     quotes = json.load(f)
-print(quotes)
 
