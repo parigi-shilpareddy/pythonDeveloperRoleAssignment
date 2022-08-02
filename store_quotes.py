02f3d7179a69bf584c4becb4d64df0deb815ddba
parigi-shilpareddy <107068930+parigi-shilpareddy@users.noreply.github.com>
2022-08-02 12:21:42 +0530

    Add files via upload

--- store_quotes.py
+++ store_quotes.py
@@ -64,31 +64,41 @@ for row in authors:
     print(row)
 for row in tags:
     print(row)
+
 cursor.execute("SELECT * FROM Quote")
 print(len(cursor.fetchall())) #output = 100
+                    #or
+TotalQuotations = cursor.execute('''SELECT COUNT(*) FROM Quote''')
+for row in TotalQuotations:
+    print(row) #output = 100
+
 
 #cursor.executemany(sqlite_insert_with_param, multiple_tags)
 cursor.execute("SELECT * FROM Quote WHERE Author = 'Albert Einstein'")
 print(len(cursor.fetchall())) #output = 10
+                    #or
+AuthorQuotationsCount = cursor.execute('''SELECT Author,COUNT(*) FROM Quote WHERE Author = "Albert Einstein"''')
+for row in AuthorQuotationsCount:
+    print(row) #output=('Albert Einstein', 10)
+
 
 max_tag_count = "SELECT MAX(tagCount) FROM Tags"
-  
 cursor.execute(max_tag_count)
-  
 print("The maximum no of tags on a quotation is:")
 print(cursor.fetchone()[0]) #output = 8
+
+
 min_tag_count = "SELECT MIN(tagCount) from Tags"
-  
 cursor.execute(min_tag_count)
-  
 print("The minimum no of tags on a quotation is:")
 print(cursor.fetchone()[0]) #output = 1
+
+
 avg_tag_count = "select avg(tagCount) from Tags"
-  
 cursor.execute(avg_tag_count)
-  
 print("The average no of tags on a quotation is:")
 print(cursor.fetchone()[0]) #output = 2.35
+
 
 #Given a number N return top N authors who authored the maximum number of tags on quotations sorted in descending order of no. of tags
 data = cursor.execute("""SELECT Quote.Qid,Quote.QuoteName,Quote.Author,Quote.Tags,Tags.tagCount
